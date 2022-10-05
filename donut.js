17d03c95c4257f8fd58b1d8707d321026c13f974
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-04 20:59:02 -0400

    Update donut.js

--- donut.js
+++ donut.js
@@ -5,8 +5,6 @@ const {sin, cos} = Math;
 const pretag = document.getElementById("d");
 
 let
-  tmr1,
-  tmr2,
   A = 1,
   B = 1;
 
@@ -21,18 +19,21 @@ const asciiframe = () => {
     sA = sin(A),
     cB = cos(B),
     sB = sin(B);
+  const m = 80
   for (let k = 0; k < 1760; k++) {
-    b[k] = k % 80 == 79 ? "\n" : " ";
+    b[k] = k % m === m - 1 ? "\n" : " ";
     z[k] = 0;
   }
-  for (let j = 0; j < 6.28; j += 0.07) {
+  const tmp0 = 6.28
+  for (let j = 0; j < tmp0; j += 0.07) {
     // j <=> theta
-    let
+    const
       ct = cos(j),
       st = sin(j);
-    for (let i = 0; i < 6.28; i += 0.02) {
+    for (let i = 0; i < tmp0; i += 0.02) {
       // i <=> phi
-      let sp = sin(i),
+      let
+        sp = sin(i),
         cp = cos(i),
         h = ct + 2, // R1 + R2*cos(theta)
         D = 1 / (sp * h * sA + st * cA + 5), // this is 1/z
@@ -41,7 +42,7 @@ const asciiframe = () => {
       let
         x = 0 | (40 + 30 * D * (cp * h * cB - t * sB)),
         y = 0 | (12 + 15 * D * (cp * h * sB + t * cB)),
-        o = x + 80 * y,
+        o = x + m * y,
         N =
           0 |
           (8 *
@@ -49,7 +50,7 @@ const asciiframe = () => {
               sp * ct * sA -
               st * cA -
               cp * ct * sB));
-      if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
+      if (y < 22 && y >= 0 && x >= 0 && x < (m - 1) && D > z[o]) {
         z[o] = D;
         b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
       }
