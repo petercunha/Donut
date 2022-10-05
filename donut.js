3be5d6a7b3491c8e82f7825299eb922fe2ea829c
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-04 21:03:15 -0400

    immutability

--- donut.js
+++ donut.js
@@ -12,7 +12,7 @@ const asciiframe = () => {
   A += 0.07;
   B += 0.03;
 
-  let
+  const
     b = [],
     z = [],
     cA = cos(A),
@@ -32,14 +32,14 @@ const asciiframe = () => {
       st = sin(j);
     for (let i = 0; i < tmp0; i += 0.02) {
       // i <=> phi
-      let
+      const
         sp = sin(i),
         cp = cos(i),
         h = ct + 2, // R1 + R2*cos(theta)
         D = 1 / (sp * h * sA + st * cA + 5), // this is 1/z
         t = sp * h * cA - st * sA; // this is a clever factoring of some of the terms in x' and y'
 
-      let
+      const
         x = 0 | (40 + 30 * D * (cp * h * cB - t * sB)),
         y = 0 | (12 + 15 * D * (cp * h * sB + t * cB)),
         o = x + m * y,
